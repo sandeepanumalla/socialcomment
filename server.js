bda2169258cf16fac28a94613ff00fefaf0cd437
Sandeep Anumalla <42109850+sandeepanumalla@users.noreply.github.com>
2021-06-12 20:48:24 +0530

    Update server.js

--- server.js
+++ server.js
@@ -8,8 +8,8 @@ app.use(express.static('client/build'));
 app.get("*",(req,res)=>{
     res.sendFile(path.resolve(__dirname,"client","build","index.html"));
 })
+const port = process.env.PORT;
 
-
-app.listen(process.env.PORT || 5000,()=>{
-    console.log(`app is running on ${process.env.PORT}`);
+app.listen(port || 5000,()=>{
+    console.log(`app is running on ${port}`);
 })
